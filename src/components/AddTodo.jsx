f2489303ab6c60d589021e0298cb2df36b544f51
amitesh <amiteshmuduli1@gmail.com>
2024-07-19 23:38:57 +0530

    disabled button styled

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -12,7 +12,7 @@ const AddTodo = () => {
             <h2 className="text-lg font-bold mb-3">Add a Task</h2>
             <form onSubmit={(e) => {handleAdd(e)}}>
                 <input onChange={handleChange} value={todo} type="text" placeholder=' Write you next task' className='p-2 pl-3 w-full rounded-full'/>
-                <div className="flex justify-center mt-3"><button type="submit" disabled={todo.length<=3} className='bg-[#415e53] hover:bg-[#183D3D] disabled:bg-[#5C8374] p-2 py-1 text-md font-semibold text-white rounded-full w-1/3'>SAVE</button></div>
+                <div className="flex justify-center mt-3"><button type="submit" disabled={todo.length<=3} className='bg-[#415e53] hover:bg-[#183D3D] disabled:bg-gray-400 disabled:text-gray-600  p-2 py-1 text-md font-semibold text-white rounded-full w-1/3'>SAVE</button></div>
             </form>
         </div>
     );
