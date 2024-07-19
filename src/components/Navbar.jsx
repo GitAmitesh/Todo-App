a774f192f9eee1495535893cd0ab707dbf4042e2
amitesh <amiteshmuduli1@gmail.com>
2024-07-19 22:00:40 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <nav className="flex justify-center bg-[#183D3D] text-[#93B1A6] py-2">
             <div className="logo">
-                <span className="font-bold text-xl mx-9">Task Manager</span>
+                <span className="font-bold text-xl mx-9">iTask</span>
             </div>
         </nav>
     );
